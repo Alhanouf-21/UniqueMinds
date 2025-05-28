1e59aeef4f0d8259df5c5343dae959e6005adf02
Alhanouf-21 <aldossari3421@gmail.com>
2025-05-29 00:05:53 +0300

    Add files via upload

---
+++ letterSound.js
@@ -0,0 +1,114 @@
+
+function A(){
+    var music = new Audio('A.OGG');
+    music.play();
+    }
+function B(){
+    var music = new Audio('B.OGG');
+    music.play();
+    }
+function C(){
+    var music = new Audio('C.OGG');
+    music.play();
+    }
+
+function D(){
+    var music = new Audio('D.OGG');
+    music.play();
+    }
+function E(){
+    var music = new Audio('E.OGG');
+    music.play();
+    }
+function F(){
+    var music = new Audio('F.OGG');
+    music.play();
+    }
+
+function G(){
+    var music = new Audio('G.OGG');
+    music.play();
+    }
+function H(){
+    var music = new Audio('H.OGG');
+    music.play();
+    }
+function I(){
+    var music = new Audio('I.OGG');
+    music.play();
+    }
+function G(){
+    var music = new Audio('G.OGG');
+    music.play();
+    }
+function K(){
+    var music = new Audio('K.OGG');
+    music.play();
+    }
+function L(){
+    var music = new Audio('L.OGG');
+    music.play();
+    }
+
+function M(){
+    var music = new Audio('M.OGG');
+    music.play();
+    }
+function N(){
+    var music = new Audio('N.OGG');
+    music.play();
+    }
+function O(){
+    var music = new Audio('O.OGG');
+    music.play();
+    }
+function P(){
+    var music = new Audio('P.OGG');
+    music.play();
+    }
+function Q(){
+    var music = new Audio('Q.OGG');
+    music.play();
+    }
+function R(){
+    var music = new Audio('R.OGG');
+    music.play();
+    }
+function S(){
+    var music = new Audio('S.OGG');
+    music.play();
+    }
+function T(){
+    var music = new Audio('T.OGG');
+    music.play();
+    }
+function U(){
+    var music = new Audio('U.OGG');
+    music.play();
+    }
+
+function V(){
+    var music = new Audio('V.OGG');
+    music.play();
+    }
+function W(){
+    var music = new Audio('W.OGG');
+    music.play();
+    }
+function X(){
+    var music = new Audio('X.OGG');
+    music.play();
+    }
+
+function Y(){
+    var music = new Audio('Y.OGG');
+    music.play();
+    }
+function Z(){
+    var music = new Audio('Z.OGG');
+    music.play();
+    }
+function J(){
+    var music = new Audio('J.OGG');
+    music.play();
+    }
